5a6afffaa5ef4196530c02352d104509e37e8a50
Evan <evan_quirk@me.com>
2021-02-10 21:50:38 +0000

    update handleUserInput to actually work. doi

--- input.js
+++ input.js
@@ -1,10 +1,12 @@
-const setupInput = function() {
+let connection;
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
-  const handleUserInput = ('data', () => {
+  const handleUserInput = process.stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
     }
