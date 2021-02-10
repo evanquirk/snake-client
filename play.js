62fd64115d2707226b20c65327674cd354b4b82f
Evan <evan_quirk@me.com>
2021-02-10 20:32:07 +0000

    add connection on and recieved data from server

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function(data) {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+    conn.on('connect', () => {
+      console.log('connected to the server');
+    })
+  conn.on('data', function(message) {
+    console.log('new message from server', message)
+  })
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
